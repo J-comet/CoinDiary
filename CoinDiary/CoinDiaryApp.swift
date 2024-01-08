2f539d87a7ba5b356d9da86d607275fa772bcc08
hyeseong <hyeseong7848@gmail.com>
2024-01-08 22:38:06 +0900

    Initial Commit

---
+++ CoinDiary/CoinDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinDiaryApp.swift
+//  CoinDiary
+//
+//  Created by 장혜성 on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
