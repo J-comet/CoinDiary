a32aaae2800c742fabbe9da1661995f32c3b1225
hyeseong <hyeseong7848@gmail.com>
2024-01-11 21:48:10 +0900

    HomeView Sticky Header 수정

--- CoinDiary/View/Home/View/HomeView.swift
+++ CoinDiary/View/Home/View/HomeView.swift
@@ -9,23 +9,23 @@ import SwiftUI
 
 /**
  * HomeViewModel 에서 상세 코인화면으로 이동시
-  - 상세화면에서도 웹소켓통신 중
-    - 첫번째시도 : HomeViewModel deinit 에서 소켓연결 끊고 DetailViewModel 에서 재연결
-        - deinit 이 init 보다 더 늦게 호출되어 결과적으로 DetailView 에서 소켓연결이 해제되어 실패
-    - 두번째시도 : 소켓 연결을 해제하지 않고 DetailCoinView 로 이동 후 웹소켓에 상세 코인의 데이터만 전송해달라고 웹소켓 Send 실행
-        - 뒤로가기후 Home 으로 돌아왔을 때는 다시 코인 목록에 대한 데이터를 전송해달라고 웹소켓 Send 실행
-        - 이 과정에서 뒤로가기에도 onAppear 는 실행되지 않는 문제가 있어 기존의 UIKit 의 ViewController 생명주기를 사용할 수 있도록 개발
+ - 상세화면에서도 웹소켓통신 중
+ - 첫번째시도 : HomeViewModel deinit 에서 소켓연결 끊고 DetailViewModel 에서 재연결
+ - deinit 이 init 보다 더 늦게 호출되어 결과적으로 DetailView 에서 소켓연결이 해제되어 실패
+ - 두번째시도 : 소켓 연결을 해제하지 않고 DetailCoinView 로 이동 후 웹소켓에 상세 코인의 데이터만 전송해달라고 웹소켓 Send 실행
+ - 뒤로가기후 Home 으로 돌아왔을 때는 다시 코인 목록에 대한 데이터를 전송해달라고 웹소켓 Send 실행
+ - 이 과정에서 뒤로가기에도 onAppear 는 실행되지 않는 문제가 있어 기존의 UIKit 의 ViewController 생명주기를 사용할 수 있도록 개발
  
  * 포그라운드 / 백그라운드 분기해서 소켓연결관리
  
  
  
-DetailCoinView() 로 이동을 했지만 여전히 소켓통신이 연결되어 있었다.
+ DetailCoinView() 로 이동을 했지만 여전히 소켓통신이 연결되어 있었다.
  - View 의 라이프사이클 onAppear , onDisapper 메서드에서 소켓을 연결/해제를 해주었다.
  - 백그라운드로 갔다가 포그라운드로 왔을 때는??
  - 백그라운드 일때 여전히 소켓통신이 진행 중...
  - onAppear 와 onDisappear 는 View 의 라이프사이클이기 때문에 View 의 생성/소멸 시점에 호출된다.
-    따라서 백그라운드/포그라운드로 진입했을 때 로그가 찍히지 않는 것을 확인했다
+ 따라서 백그라운드/포그라운드로 진입했을 때 로그가 찍히지 않는 것을 확인했다
  - 백그라운드/포그라운드는 App 의 라이프사이클에서 확인 가능 하다.
  - 결국 App 의 라이프사이클과 View 의 라이프사이클 모두에 소켓통신을 연결/해제를 해주는 코드를 작성해서 해결
  */
@@ -58,7 +58,6 @@ struct HomeView: View {
                             width: geometry.size.width,
                             height: headerMinHeight + (offset > 0 ? offset : 0)
                         )
-                        .frame(maxHeight: headerMinHeight)
                         .offset(y: (offset > 0 ? -offset : 0))
                 }
                 .frame(minHeight: headerMinHeight)
@@ -76,31 +75,30 @@ struct HomeView: View {
                                 
                             }
                             .buttonStyle(PlainButtonStyle())   // HomeRowView 내부 텍스트 컬러값 적용
-
+                            
                         }
                     }
                 }
                 .background(Color(.systemGray6))
             }
-//            .task {
-//                print("HomeView Task")
-//                WebSocketManager.shared.openWebSocket()
-//                viewModel.fetchAllMarket()                
-//            }
-//            .onAppear {
-//                print("HomeView onAppear")
-////                WebSocketManager.shared.openWebSocket()
-//                viewModel.fetchAllMarket()
-//            }
-//            .onDisappear {
-//                print("HomeView onDisappear")
-//                // 상세화면에서도 웹소켓을 계속 진행할 예정이라 close 코드 제거
-////                WebSocketManager.shared.closeWebSocket()
-//            }
+            //            .task {
+            //                print("HomeView Task")
+            //                WebSocketManager.shared.openWebSocket()
+            //                viewModel.fetchAllMarket()
+            //            }
+            //            .onAppear {
+            //                print("HomeView onAppear")
+            ////                WebSocketManager.shared.openWebSocket()
+            //                viewModel.fetchAllMarket()
+            //            }
+            //            .onDisappear {
+            //                print("HomeView onDisappear")
+            //                // 상세화면에서도 웹소켓을 계속 진행할 예정이라 close 코드 제거
+            ////                WebSocketManager.shared.closeWebSocket()
+            //            }
             .overlay(
                 Rectangle()
-                    .foregroundStyle(Color(uiColor: .white))
-//                    .foregroundStyle(Color(uiColor: .systemGray6))
+                    .foregroundStyle(Color(uiColor: .systemGray6))
                     .frame(height: UIApplication.shared.currentUIWindow()?.safeAreaInsets.top)
                     .edgesIgnoringSafeArea(.all)
                     .opacity(offsetY > -headerMinHeight ? 0 : 1)
